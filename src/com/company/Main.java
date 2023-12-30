39cfccd2020ea9a83001009ce2466b588c1bd4fc
Rahayu Usnul <73322659+rahayuusnul@users.noreply.github.com>
2023-12-30 09:37:03 +0700

    Add files via upload

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main{
+    public static void main(String[]args){
+        int i = 0;
+        int x = 0;
+        System.out.print("Deret: ");
+        while(x<20){
+            if (i%3==0)
+                i++;
+            x = 2 * i;
+            System.out.print(" "+ x +", ");
+            i++;}
+        System.out.println(" ");
+    }
+}
